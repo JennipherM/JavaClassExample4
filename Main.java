ab0bcadf832dd3d3a5ecc4bb084095f845fb0c5a
Jennipher <jenniphermedina11@gmail.com>
2025-03-26 13:59:35 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,42 @@
+/*
+create rock, paper, scissors
+
+*/
+import java.util.Random;
+import java.util.Scanner; 
+
+enum Choice
+        {
+            rock,
+            paper,
+            scissors
+        }
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in); 
+        System.out.println("Enter rock, paper, or scissors(or exit to quit)");
+        Random random = new Random();
+
+        String userInput = input.nextLine().toLowerCase();
+
+        Choice userChoice = Choice.valueOf(userInput);
+        Choice compChoice = Choice.values()[random.nextInt(2)];
+
+        if (userChoice == compChoice )
+        {
+            System.out.println("Its a tie");
+        }
+        else if (userChoice == Choice.rock && compChoice == Choice.paper ||
+                 userChoice == Choice.paper && compChoice == Choice.scissors || 
+                 userChoice == Choice.scissors && compChoice == Choice.rock)
+        {
+            System.out.println("you lose");
+        }
+        else 
+        {
+            System.out.println("you win");
+        }
+    }
+}
